66c7aa38c757a32540b64c52d11d783d1b570d33
Your Name <you@example.com>
2022-11-15 18:38:32 +0100

    ...

---
+++ twitter/twitter20221115/data/deleted-tweet-headers.js
@@ -0,0 +1,42 @@
+window.YTD.deleted_tweet_headers.part0 = [
+  {
+    "tweet" : {
+      "tweet_id" : "1591765198737920002",
+      "user_id" : "1410298424910561285",
+      "created_at" : "Sun Nov 13 12:09:31 +0000 2022",
+      "deleted_at" : "Sun Nov 13 12:15:11 +0000 2022"
+    }
+  },
+  {
+    "tweet" : {
+      "tweet_id" : "1589242120329515009",
+      "user_id" : "1410298424910561285",
+      "created_at" : "Sun Nov 06 13:03:42 +0000 2022",
+      "deleted_at" : "Sun Nov 06 13:09:49 +0000 2022"
+    }
+  },
+  {
+    "tweet" : {
+      "tweet_id" : "1589157136134983682",
+      "user_id" : "1410298424910561285",
+      "created_at" : "Sun Nov 06 07:26:01 +0000 2022",
+      "deleted_at" : "Sun Nov 06 07:26:56 +0000 2022"
+    }
+  },
+  {
+    "tweet" : {
+      "tweet_id" : "1587741857978662912",
+      "user_id" : "1410298424910561285",
+      "created_at" : "Wed Nov 02 09:42:12 +0000 2022",
+      "deleted_at" : "Wed Nov 02 09:43:04 +0000 2022"
+    }
+  },
+  {
+    "tweet" : {
+      "tweet_id" : "1587558241021689857",
+      "user_id" : "1410298424910561285",
+      "created_at" : "Tue Nov 01 21:32:34 +0000 2022",
+      "deleted_at" : "Tue Nov 01 21:33:04 +0000 2022"
+    }
+  }
+]
---
+++ twitter/twitter20221115/data/deleted-tweets.js
@@ -0,0 +1,355 @@
+window.YTD.deleted_tweets.part0 = [
+  {
+    "tweet" : {
+      "edit_info" : {
+        "initial" : {
+          "editTweetIds" : [
+            "1591765198737920002"
+          ],
+          "editableUntil" : "2022-11-13T12:39:31.000Z",
+          "editsRemaining" : "5",
+          "isEditEligible" : false
+        }
+      },
+      "retweeted" : false,
+      "source" : "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+      "entities" : {
+        "hashtags" : [ ],
+        "symbols" : [ ],
+        "user_mentions" : [
+          {
+            "name" : "Kapo_Aargau",
+            "screen_name" : "Kapo_Aargau",
+            "indices" : [
+              "203",
+              "215"
+            ],
+            "id_str" : "761560190105841664",
+            "id" : "761560190105841664"
+          },
+          {
+            "name" : "fedpol",
+            "screen_name" : "fedpolCH",
+            "indices" : [
+              "255",
+              "264"
+            ],
+            "id_str" : "733318991230476289",
+            "id" : "733318991230476289"
+          },
+          {
+            "name" : "WikiLeaks",
+            "screen_name" : "wikileaks",
+            "indices" : [
+              "265",
+              "275"
+            ],
+            "id_str" : "16589206",
+            "id" : "16589206"
+          },
+          {
+            "name" : "Parl CH",
+            "screen_name" : "ParlCH",
+            "indices" : [
+              "276",
+              "283"
+            ],
+            "id_str" : "3165953063",
+            "id" : "3165953063"
+          }
+        ],
+        "urls" : [ ]
+      },
+      "display_text_range" : [
+        "0",
+        "283"
+      ],
+      "favorite_count" : "0",
+      "in_reply_to_status_id_str" : "1591746199853441025",
+      "id_str" : "1591765198737920002",
+      "in_reply_to_user_id" : "1410298424910561285",
+      "truncated" : false,
+      "retweet_count" : "0",
+      "id" : "1591765198737920002",
+      "in_reply_to_status_id" : "1591746199853441025",
+      "created_at" : "Sun Nov 13 12:09:31 +0000 2022",
+      "favorited" : false,
+      "full_text" : "ÜBERMITTLUNG:\nDie Zürich Versicherung [LOCK] [REDIR] [LOCK] benutzt die Drogensüchtige für Auftragsmorde\n\n[INSERT] oder um z.B. Beweismittel zu vernichten wie mit dem Sohn des ehem. Polizeichefs Frehner @Kapo_Aargau als Junkie &lt;- jemand aus Aarau\n\nCC: @fedpolCH @wikileaks @ParlCH",
+      "lang" : "de",
+      "in_reply_to_screen_name" : "PinkyDef",
+      "in_reply_to_user_id_str" : "1410298424910561285",
+      "deleted_at" : "Sun Nov 13 12:15:11 +0000 2022"
+    }
+  },
+  {
+    "tweet" : {
+      "edit_info" : {
+        "initial" : {
+          "editTweetIds" : [
+            "1589242120329515009"
+          ],
+          "editableUntil" : "2022-11-06T13:33:42.000Z",
+          "editsRemaining" : "5",
+          "isEditEligible" : false
+        }
+      },
+      "retweeted" : false,
+      "source" : "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+      "entities" : {
+        "hashtags" : [ ],
+        "symbols" : [ ],
+        "user_mentions" : [
+          {
+            "name" : "fedpol",
+            "screen_name" : "fedpolCH",
+            "indices" : [
+              "269",
+              "278"
+            ],
+            "id_str" : "733318991230476289",
+            "id" : "733318991230476289"
+          }
+        ],
+        "urls" : [ ]
+      },
+      "display_text_range" : [
+        "0",
+        "278"
+      ],
+      "favorite_count" : "0",
+      "in_reply_to_status_id_str" : "1589241594150281216",
+      "id_str" : "1589242120329515009",
+      "in_reply_to_user_id" : "1410298424910561285",
+      "truncated" : false,
+      "retweet_count" : "0",
+      "id" : "1589242120329515009",
+      "in_reply_to_status_id" : "1589241594150281216",
+      "created_at" : "Sun Nov 06 13:03:42 +0000 2022",
+      "favorited" : false,
+      "full_text" : "UNBEKANNT:\n\"Ich würde den Belser erpressen\"\n\nich:\nalso gemäss Gesetz dürfte eine Polizist gar nicht Polizist werden wenn er Drogen konsumiert hat. Ich glaub da liegt der Fehler nicht bei mir der den Drogenkonsum von Markus Belser auf vor Gericht beschwören könnte\n\nCC: @fedpolCH",
+      "lang" : "de",
+      "in_reply_to_screen_name" : "PinkyDef",
+      "in_reply_to_user_id_str" : "1410298424910561285",
+      "deleted_at" : "Sun Nov 06 13:09:49 +0000 2022"
+    }
+  },
+  {
+    "tweet" : {
+      "edit_info" : {
+        "initial" : {
+          "editTweetIds" : [
+            "1589157136134983682"
+          ],
+          "editableUntil" : "2022-11-06T07:56:01.000Z",
+          "editsRemaining" : "5",
+          "isEditEligible" : false
+        }
+      },
+      "retweeted" : false,
+      "source" : "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+      "entities" : {
+        "hashtags" : [ ],
+        "symbols" : [ ],
+        "user_mentions" : [ ],
+        "urls" : [ ]
+      },
+      "display_text_range" : [
+        "0",
+        "102"
+      ],
+      "favorite_count" : "0",
+      "in_reply_to_status_id_str" : "1589156626346704896",
+      "id_str" : "1589157136134983682",
+      "in_reply_to_user_id" : "1410298424910561285",
+      "truncated" : false,
+      "retweet_count" : "0",
+      "id" : "1589157136134983682",
+      "in_reply_to_status_id" : "1589156626346704896",
+      "created_at" : "Sun Nov 06 07:26:01 +0000 2022",
+      "favorited" : false,
+      "full_text" : "oder an der Fleisch-Theke typsich schweizerisch:\n\nDarf es ein bisschen mehr sein (vom \"Pferd\"efleisch)",
+      "lang" : "de",
+      "in_reply_to_screen_name" : "PinkyDef",
+      "in_reply_to_user_id_str" : "1410298424910561285",
+      "deleted_at" : "Sun Nov 06 07:26:56 +0000 2022"
+    }
+  },
+  {
+    "tweet" : {
+      "edit_info" : {
+        "initial" : {
+          "editTweetIds" : [
+            "1587741857978662912"
+          ],
+          "editableUntil" : "2022-11-02T10:12:12.000Z",
+          "editsRemaining" : "5",
+          "isEditEligible" : true
+        }
+      },
+      "retweeted" : false,
+      "source" : "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+      "entities" : {
+        "hashtags" : [
+          {
+            "text" : "Autist",
+            "indices" : [
+              "3",
+              "10"
+            ]
+          },
+          {
+            "text" : "Bällebad",
+            "indices" : [
+              "14",
+              "23"
+            ]
+          },
+          {
+            "text" : "Autismus",
+            "indices" : [
+              "234",
+              "243"
+            ]
+          },
+          {
+            "text" : "Burnout",
+            "indices" : [
+              "254",
+              "262"
+            ]
+          }
+        ],
+        "symbols" : [ ],
+        "user_mentions" : [
+          {
+            "name" : "Universität Basel",
+            "screen_name" : "UniBasel",
+            "indices" : [
+              "268",
+              "277"
+            ],
+            "id_str" : "141164574",
+            "id" : "141164574"
+          },
+          {
+            "name" : "Universität Bern",
+            "screen_name" : "unibern",
+            "indices" : [
+              "278",
+              "286"
+            ],
+            "id_str" : "60514114",
+            "id" : "60514114"
+          }
+        ],
+        "urls" : [ ]
+      },
+      "display_text_range" : [
+        "0",
+        "286"
+      ],
+      "favorite_count" : "0",
+      "id_str" : "1587741857978662912",
+      "truncated" : false,
+      "retweet_count" : "0",
+      "id" : "1587741857978662912",
+      "created_at" : "Wed Nov 02 09:42:12 +0000 2022",
+      "favorited" : false,
+      "full_text" : "1x #Autist\n1x #Bällebad mit vielen ungelösten Rubik Cubes\n\n= denial of service Attack gegen den Autisten\n\nAls Metapher betrachtet:\nganze Welt ist eigentlich Bällebad mit tonnenweise ungelösten Rubik Würfeln,nicht nur der CO2 Würfel.\n\n#Autismus &lt;-&gt; #Burnout\n\nCC: @UniBasel @unibern",
+      "lang" : "de",
+      "deleted_at" : "Wed Nov 02 09:43:04 +0000 2022"
+    }
+  },
+  {
+    "tweet" : {
+      "edit_info" : {
+        "initial" : {
+          "editTweetIds" : [
+            "1587558241021689857"
+          ],
+          "editableUntil" : "2022-11-01T22:02:34.000Z",
+          "editsRemaining" : "5",
+          "isEditEligible" : false
+        }
+      },
+      "retweeted" : false,
+      "source" : "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+      "entities" : {
+        "hashtags" : [ ],
+        "symbols" : [ ],
+        "user_mentions" : [
+          {
+            "name" : "NSA/CSS",
+            "screen_name" : "NSAGov",
+            "indices" : [
+              "156",
+              "163"
+            ],
+            "id_str" : "2248872301",
+            "id" : "2248872301"
+          },
+          {
+            "name" : "CIA",
+            "screen_name" : "CIA",
+            "indices" : [
+              "166",
+              "170"
+            ],
+            "id_str" : "2359926157",
+            "id" : "2359926157"
+          },
+          {
+            "name" : "fedpol",
+            "screen_name" : "fedpolCH",
+            "indices" : [
+              "248",
+              "257"
+            ],
+            "id_str" : "733318991230476289",
+            "id" : "733318991230476289"
+          },
+          {
+            "name" : "Kanton Aargau",
+            "screen_name" : "kantonaargau",
+            "indices" : [
+              "258",
+              "271"
+            ],
+            "id_str" : "467381354",
+            "id" : "467381354"
+          },
+          {
+            "name" : "Bundesgericht",
+            "screen_name" : "bger_CH",
+            "indices" : [
+              "272",
+              "280"
+            ],
+            "id_str" : "3906545003",
+            "id" : "3906545003"
+          }
+        ],
+        "urls" : [ ]
+      },
+      "display_text_range" : [
+        "0",
+        "280"
+      ],
+      "favorite_count" : "0",
+      "in_reply_to_status_id_str" : "1587557851261591552",
+      "id_str" : "1587558241021689857",
+      "in_reply_to_user_id" : "1410298424910561285",
+      "truncated" : false,
+      "retweet_count" : "0",
+      "id" : "1587558241021689857",
+      "in_reply_to_status_id" : "1587557851261591552",
+      "created_at" : "Tue Nov 01 21:32:34 +0000 2022",
+      "favorited" : false,
+      "full_text" : "und falls dem so wäre wäre das vermutlich jemand der sich richtig gut mit netzwerken auskennt, dies seit mindestens/fast 20 Jahren. Also wäre Pfisterer ein @NSAGov / @CIA Agent der Zeugen für die Wirtschaft ober Oberschichtschnösels \"abräumt\"\n\nCC: @fedpolCH @kantonaargau @bger_CH",
+      "lang" : "de",
+      "in_reply_to_screen_name" : "PinkyDef",
+      "in_reply_to_user_id_str" : "1410298424910561285",
+      "deleted_at" : "Tue Nov 01 21:33:04 +0000 2022"
+    }
+  }
+]
